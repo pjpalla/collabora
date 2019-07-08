f3abb3987b6219fc1c3f7c3a87651ae7e6bff41e
Pg <palla.pg@gmail.com>
2019-07-08 17:29:44 +0000

    change to manage ajax calls and js.coffee files rendering

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,21 +1,43 @@
 class UsersController < ApplicationController
   def index
+    # @msg = { "success" => "true", "message" => "hello"}
+ 
+   
     @users = User.all
-    @locations = User.distinct.pluck(:place) +  ["cagliari", "carbonia", "nuoro", "sassari","sardegna"]
+    @locations = User.distinct.pluck(:place) +  ["cagliari", "oristano", "nuoro", "sassari","sardegna", "sardegna eccetto medio campidano", "altro"]
+    
+    @location = "Medio Campidano"
+    if params[:locations]
+      @location = params[:locations]
+    end  
+    
+    
+    logger.debug("location param: #{@location}")
+    #@locations = @locations.compact!
+    #binding.pry
     
     all_users = User.count
     @males = User.where(sex: 'M').count
     @females = all_users - @males
     
     respond_to do |format|
-      format.html
-      format.js
-    end  
+       if request.xhr?
+          logger.debug "XHR request"
+          logger.debug "ajax param: #{@location}"
+          format.js 
+       else  
+        format.html
+       end  
+
+    end
+    #   format.json { render json: @msg }
+    end
+end    
     
-  end
+    
+
   
   # def users_by_age
   
   # end  
   
-end
