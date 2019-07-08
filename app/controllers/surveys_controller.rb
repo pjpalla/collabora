aa094f54dc4cd0bb65bfdbe4cdc3bdff57f194de
Pg <palla.pg@gmail.com>
2019-07-08 17:27:08 +0000

    change survey controller

--- app/controllers/surveys_controller.rb
+++ app/controllers/surveys_controller.rb
@@ -33,6 +33,8 @@ class SurveysController < ApplicationController
   # POST /surveys.json
   def create
     @questions = Question.all
+    #@questions = questions.sort{|u, w| u.qid <=> w.qid}
+    
     session[:survey_params].deep_merge!(params[:survey]) if params[:survey]
     self.set_session
     @survey = Survey.new(session[:survey_params])
