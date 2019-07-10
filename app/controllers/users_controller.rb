e75d364ac8b77b147efa27accd16f80da0ba90d9
Pg <palla.pg@gmail.com>
2019-07-10 17:10:38 +0000

    add the business logic to manage the different geographic regions

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,22 +3,34 @@ class UsersController < ApplicationController
     # @msg = { "success" => "true", "message" => "hello"}
  
    
-    @users = User.all
-    @locations = User.distinct.pluck(:place) +  ["cagliari", "oristano", "nuoro", "sassari","sardegna", "sardegna eccetto medio campidano", "altro"]
+    @locations = ["medio campidano", "cagliari", "oristano", "nuoro", "sassari","sardegna", "sardegna eccetto medio campidano", "altro"]
+    #@locations = User.distinct.pluck(:place) + @basic_locations
+    #@locations.uniq!
     
-    @location = "Medio Campidano"
+    @location = "medio campidano"
+    
     if params[:locations]
-      @location = params[:locations]
+      @location = params[:locations].downcase!
+      logger.debug "@location: #{@location}"
     end  
     
+    @users = User.where(place: @location)
+    if @location == "sardegna"
+      @users = User.all
+    elsif @location == "sardegna eccetto medio campidano"
+      @users = User.where.not(place: 'medio campidano')
+      logger.debug "eccetto: #{@users.length}"
+    elsif @location == "altro"
+      @users = User.where.not(place: @locations)
+    end  
     
     logger.debug("location param: #{@location}")
     #@locations = @locations.compact!
     #binding.pry
     
-    all_users = User.count
-    @males = User.where(sex: 'M').count
-    @females = all_users - @males
+    # all_users = User.where(place: @location).count
+    # @males = User.where(place: @location, sex: 'M').count
+    # @females = all_users - @males
     
     respond_to do |format|
        if request.xhr?
