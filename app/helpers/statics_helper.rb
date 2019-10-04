ec09a0e55cc89b0d7927572409c0f3f7198105ff
Pg <palla.pg@gmail.com>
2019-10-04 14:37:28 +0000

    extend method clean_counts in the statistics helper

--- app/helpers/statics_helper.rb
+++ app/helpers/statics_helper.rb
@@ -315,11 +315,13 @@ end
     
     
     
-    def clean_counts(counts)
+  def clean_counts(counts)
       l = []
       h = Hash.new
       counts.each do |k,v|
-            tmp_key = k.gsub(" / ", "/")
+            tmp_key = k.gsub(/\s*\/\s*/, '/')
+            tmp_key = tmp_key.gsub(/\s*</, "< ")
+            tmp_key = tmp_key.gsub(/\s*>/, "> ")            
             tmp_key = tmp_key.gsub("  ", " ")
             if h.keys.include?(tmp_key)
                 h[tmp_key] += v
